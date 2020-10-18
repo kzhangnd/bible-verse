a88b747f551c7bbeb7cc719eddc71cbab47840af
Kai Zhang <kzhang4@student04.cse.nd.edu>
2020-10-18 00:41:41 -0400

    finish test files

---
+++ server/test_bible_get.py
@@ -0,0 +1,98 @@
+import unittest
+import requests
+import json
+
+class TestMovies(unittest.TestCase):
+
+    SITE_URL = 'http://student04.cse.nd.edu:51060' # replace with your port number and 
+    print("testing for server: " + SITE_URL)
+    BOOK_URL = SITE_URL + '/book/'
+    CHAPTER_URL = SITE_URL + '/chapter/'
+    VERSE_URL = SITE_URL + '/verse/'
+
+    def is_json(self, resp):
+        try:
+            json.loads(resp)
+            return True
+        except ValueError:
+            return False
+
+    def test_book_get(self):
+        r = requests.get(self.BOOK_URL)
+        self.assertTrue(self.is_json(r.content.decode('utf-8')))
+        resp = json.loads(r.content.decode('utf-8'))
+        self.assertEqual(resp['result'], 'success')
+        self.assertEqual(len(resp['book']), 150)
+        
+    def test_book_search_term(self):
+        term = 'flee'
+        r = requests.get(self.BOOK_URL + term)
+        self.assertTrue(self.is_json(r.content.decode('utf-8')))
+        resp = json.loads(r.content.decode('utf-8'))
+        self.assertEqual(resp['result'], 'success')
+        self.assertEqual(resp['result_number'], 6)
+
+        term = 'Flee as a bird to your mountain '
+        r = requests.get(self.BOOK_URL + term)
+        self.assertTrue(self.is_json(r.content.decode('utf-8')))
+        resp = json.loads(r.content.decode('utf-8'))
+        self.assertEqual(resp['result'], 'success')
+        self.assertEqual(resp['search_result'][0][2], 'In the LORD put I my trust: how say ye to my soul, Flee as a bird to your mountain?')
+        self.assertEqual(resp['result_number'], 1)
+
+        term = 'f*ck'
+        r = requests.get(self.BOOK_URL + term)
+        self.assertTrue(self.is_json(r.content.decode('utf-8')))
+        resp = json.loads(r.content.decode('utf-8'))
+        self.assertEqual(resp['result'], 'success')
+        self.assertEqual(resp['result_number'], 0)
+
+    def test_chapter_get(self):
+        chapter_number = 144
+        r = requests.get(self.CHAPTER_URL + str(chapter_number))
+        self.assertTrue(self.is_json(r.content.decode('utf-8')))
+        resp = json.loads(r.content.decode('utf-8'))
+        self.assertEqual(resp['result'], 'success')
+        self.assertEqual(resp['chapter_number'], chapter_number)
+        self.assertEqual(len(resp['chapter']), 15)
+
+        chapter_number = 1440
+        r = requests.get(self.CHAPTER_URL + str(chapter_number))
+        self.assertTrue(self.is_json(r.content.decode('utf-8')))
+        resp = json.loads(r.content.decode('utf-8'))
+        self.assertEqual(resp['result'], 'error')
+        self.assertEqual(resp['chapter_number'], chapter_number)
+        self.assertEqual(resp['message'], 'chapter number out of range')
+
+    def test_verse_get(self):
+        chapter_number = 144
+        verse_number = 3
+        verse = 'LORD, what is man, that thou takest knowledge of him! or the son of man, that thou makest account of him!'
+        r = requests.get(self.VERSE_URL + str(chapter_number) + '_' + str(verse_number))
+        resp = json.loads(r.content.decode('utf-8'))
+        self.assertEqual(resp['result'], 'success')
+        self.assertEqual(resp['chapter_number'], chapter_number)
+        self.assertEqual(resp['verse_number'], verse_number)
+        self.assertEqual(resp['verse'], verse)
+
+        chapter_number = 144
+        verse_number = 33
+        r = requests.get(self.VERSE_URL + str(chapter_number) + '_' + str(verse_number))
+        resp = json.loads(r.content.decode('utf-8'))
+        self.assertEqual(resp['result'], 'error')
+        self.assertEqual(resp['chapter_number'], chapter_number)
+        self.assertEqual(resp['verse_number'], verse_number)
+        self.assertEqual(resp['message'], 'verse number out of range')
+
+        chapter_number = 1444
+        verse_number = 33
+        r = requests.get(self.VERSE_URL + str(chapter_number) + '_' + str(verse_number))
+        resp = json.loads(r.content.decode('utf-8'))
+        self.assertEqual(resp['result'], 'error')
+        self.assertEqual(resp['chapter_number'], chapter_number)
+        self.assertEqual(resp['verse_number'], verse_number)
+        self.assertEqual(resp['message'], 'chapter number out of range')
+
+if __name__ == "__main__":
+    unittest.main()
+
---
+++ server/test_favorite_module.py
@@ -0,0 +1,111 @@
+import unittest
+import requests
+import json
+
+class TestMovies(unittest.TestCase):
+
+    SITE_URL = 'http://student04.cse.nd.edu:51060' # replace with your port number and 
+    print("testing for server: " + SITE_URL)
+    FAV_URL = SITE_URL + '/favorite/'
+
+    def is_json(self, resp):
+        try:
+            json.loads(resp)
+            return True
+        except ValueError:
+            return False
+
+    def test01_fav_index_post(self):
+        # post a valid verse
+        requests.delete(self.FAV_URL)
+        m = {'chapter_number': 144, 'verse_number': 3}
+        r = requests.post(self.FAV_URL, data= json.dumps(m))
+        self.assertTrue(self.is_json(r.content.decode('utf-8')))
+        resp = json.loads(r.content.decode('utf-8'))
+        self.assertEqual(resp['result'], 'success')
+
+        # try to repeat posting
+        m = {'chapter_number': 144, 'verse_number': 3}
+        r = requests.post(self.FAV_URL, data= json.dumps(m))
+        self.assertTrue(self.is_json(r.content.decode('utf-8')))
+        resp = json.loads(r.content.decode('utf-8'))
+        self.assertEqual(resp['result'], 'error')
+        self.assertEqual(resp['message'], 'this verse is already in my favorite')
+
+        # give invalid verse number
+        m = {'chapter_number': 144, 'verse_number': 100}
+        r = requests.post(self.FAV_URL, data= json.dumps(m))
+        self.assertTrue(self.is_json(r.content.decode('utf-8')))
+        resp = json.loads(r.content.decode('utf-8'))
+        self.assertEqual(resp['result'], 'error')
+        self.assertEqual(resp['message'], 'verse number out of range')
+
+        # give invalid chapter number
+        m = {'chapter_number': 155, 'verse_number': 3}
+        r = requests.post(self.FAV_URL, data= json.dumps(m))
+        self.assertTrue(self.is_json(r.content.decode('utf-8')))
+        resp = json.loads(r.content.decode('utf-8'))
+        self.assertEqual(resp['result'], 'error')
+        self.assertEqual(resp['message'], 'chapter number out of range')
+    
+    def test02_index_get(self):
+        r = requests.get(self.FAV_URL)
+        self.assertTrue(self.is_json(r.content.decode('utf-8')))
+        resp = json.loads(r.content.decode('utf-8'))
+        self.assertEqual(resp['result'], 'success')
+        self.assertEqual(len(resp['favorite']), 1)
+
+
+    def test03_index_delete(self):
+        # try an invalid input
+        m = {}
+        chapter_number = 144
+        verse_number = 3
+        r = requests.delete(self.FAV_URL + str(chapter_number) + str(verse_number), data=json.dumps(m))
+        self.assertTrue(self.is_json(r.content.decode('utf-8')))
+        resp = json.loads(r.content.decode('utf-8'))
+        self.assertEqual(resp['result'], 'error')
+        self.assertEqual(resp['message'], 'invalid input')
+
+        # a valid and successful try
+        m = {}
+        chapter_number = 144
+        verse_number = 3
+        r = requests.delete(self.FAV_URL + str(chapter_number) + '_' + str(verse_number), data=json.dumps(m))
+        self.assertTrue(self.is_json(r.content.decode('utf-8')))
+        resp = json.loads(r.content.decode('utf-8'))
+        self.assertEqual(resp['result'], 'success')
+
+        # give invalid chapter number
+        m = {}
+        chapter_number = 1444
+        verse_number = 3
+        r = requests.delete(self.FAV_URL + str(chapter_number) + '_' + str(verse_number), data=json.dumps(m))
+        self.assertTrue(self.is_json(r.content.decode('utf-8')))
+        resp = json.loads(r.content.decode('utf-8'))
+        self.assertEqual(resp['result'], 'error')
+        self.assertEqual(resp['message'], 'chapter number out of range')
+
+        # give invalid verse number
+        m = {}
+        chapter_number = 144
+        verse_number = 300
+        r = requests.delete(self.FAV_URL + str(chapter_number) + '_' + str(verse_number), data=json.dumps(m))
+        self.assertTrue(self.is_json(r.content.decode('utf-8')))
+        resp = json.loads(r.content.decode('utf-8'))
+        self.assertEqual(resp['result'], 'error')
+        self.assertEqual(resp['message'], 'verse number out of range')
+
+        # delete a verse that is not in "my favorite"
+        m = {}
+        chapter_number = 144
+        verse_number = 3
+        r = requests.delete(self.FAV_URL + str(chapter_number) + '_' + str(verse_number), data=json.dumps(m))
+        self.assertTrue(self.is_json(r.content.decode('utf-8')))
+        resp = json.loads(r.content.decode('utf-8'))
+        self.assertEqual(resp['result'], 'error')
+        self.assertEqual(resp['message'], 'this verse is not in my favorite')
+
+if __name__ == "__main__":
+    unittest.main()
+
