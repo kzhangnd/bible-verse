805ca0fdf6dbff17b516ce30bd351f00b1080abf
Crystal Colón <ccolon2@nd.edu>
2020-10-26 17:32:55 +0000

    Update request-sending-script.js

--- daily14/script/request-sending-script.js
+++ daily14/script/request-sending-script.js
@@ -8,13 +8,13 @@ send_button.onmouseup = send_request;
 
 function send_request(){
 
-  console.log('send_request');
+    console.log('send_request');
     // set up url
     var requester        = new XMLHttpRequest(); // 1 - creating request object // the thing that does the requesting
     var url              = select.value;
     var port             = document.getElementById("input-port-number");
     var key_checkbox     = document.getElementById("checkbox-use-key");
-    var message_checkbox = document.getElementsByClassName("checkbox-use-message");
+    var message_checkbox = document.getElementById("checkbox-use-message");
     var get              = document.getElementById("radio-get");
     var put              = document.getElementById("radio-put");
     var post             = document.getElementById("radio-post");
@@ -23,30 +23,26 @@ function send_request(){
 
     var total_url = url + ":" + port.value + "/movies/";
     var key = document.getElementById("input-key")
-    if(key_checkbox.checked){
+    if (key_checkbox.checked){
         total_url = total_url + key.value
     }
 
-    if(get.checked){
-      requester.open("GET", total_url, true); // 2 - associates request attributes with requester
+    if (get.checked){
+        requester.open("GET", total_url, true); // 2 - associates request attributes with requester
+
+    } else if (put.checked){
+        requester.open("PUT", total_url, true); // 2 - associates request attributes with requester
+
+    } else if (post.checked){
+        requester.open("POST", total_url, true); // 2 - associates request attributes with requester
+
+    } else { 
+        requester.open("DELETE", total_url, true); // 2 - associates request attributes with requester
 
     }
-    if(put.checked){
-      requester.open("PUT", total_url, true); // 2 - associates request attributes with requester
-
-    }
-    if(post.checked){
-      requester.open("POST", total_url, true); // 2 - associates request attributes with requester
-
-    }
-    if(delete_item.checked){
-      requester.open("DELETE", total_url, true); // 2 - associates request attributes with requester
-
-    }
-
 
     // set up onload
-    console.log("before xh.onload() in makeNetworkCallToAgeApi");
+    console.log("before xh.onload() in send_request");
 
     requester.onload = function(e) { // triggered when response is received
         var resp = document.getElementById("answer-label");
@@ -63,19 +59,20 @@ function send_request(){
     }//end of onload
 
     requester.onerror = function(e) {
-      console.error(requester.statusText)
+        console.error(requester.statusText)
     }//end of onerror
 
-
+    
 
     var message_textbox = document.getElementById("text-message-body");
-
-    if(message_checkbox.checked){
+    
+    if (document.getElementById("checkbox-use-message").checked) {
+        console.log("message on");
+        console.log(message_textbox.value);
         requester.send(message_textbox.value); //this actually makes the network call
     }
-    else{
+    else {
+        console.log("message off");
         requester.send(null); //this actually makes the network call
     }
-
-
 }
