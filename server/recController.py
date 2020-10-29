c1767bc2e6ae4cd58fe8f216f8df5bdc1913654b
Kai Zhang <kaizhang@Kais-MacBook-Pro.local>
2020-10-28 20:07:16 -0400

    finish recController

---
+++ server/recController.py
@@ -0,0 +1,26 @@
+import cherrypy
+import json
+from bible_library import _bible_database
+class RecController(object):
+
+    def __init__(self, bdb=None):
+        if bdb is None:
+            self.bdb = _bible_database()
+        else:
+            self.bdb = bdb
+
+    # when GET request for /recommendation/ comes in, then we repsond with json string
+    def GET_KEY(self, number):
+        number = int(number)
+        output = {'result': 'success'}
+        try:
+            output['recommendation'] = self.bdb.get_recommendation(number)
+            if number > self.bdb.total: # notice we cannot provide enough recommendations
+                output['result'] = 'error'
+                output['message'] = 'Not enough recommendations'
+
+        except Exception as ex:
+            output['result'] = 'error'
+            output['message'] = str(ex)
+
+        return json.dumps(output)
