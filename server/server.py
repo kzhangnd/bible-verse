67f0feece129d38e5a2f3cbedf283119faa46e38
ccolon2 <ccolon2@nd.edu>
2020-11-01 18:50:07 -0500

    ADD CORS

--- server/server.py
+++ server/server.py
@@ -11,6 +11,15 @@ from favController import FavController
 from recController import RecController
 from bible_library import _bible_database
 
+class optionsController:
+    def OPTIONS(self, *args, **kwargs):
+        return ""
+
+def CORS():
+    cherrypy.response.headers["Access-Control-Allow-Origin"] = "*"
+    cherrypy.response.headers["Access-Control-Allow-Methods"] = "GET, PUT, POST, DELETE, OPTIONS"
+    cherrypy.response.headers["Access-Control-Allow-Credentials"] = "true"
+
 
 def start_service():
     dispatcher = cherrypy.dispatch.RoutesDispatcher()
@@ -36,15 +45,27 @@ def start_service():
     dispatcher.connect('fav_index_delete', '/favorite/', controller=favController, action = 'DELETE_INDEX', conditions=dict(method=['DELETE']))
 
     dispatcher.connect('recommendation_get', '/recommendation/:number', controller=recController, action = 'GET_KEY', conditions=dict(method=['GET']))
+    #CORS related options connections
+    dispatcher.connect('book_get_options', '/book/', controller=optionsController, action = 'OPTIONS', conditions=dict(method=['OPTIONS']))
+
+    dispatcher.connect('book_search_term_options', '/book/:term', controller=optionsController, action = 'OPTIONS', conditions=dict(method=['OPTIONS']))
+    dispatcher.connect('chapter_get_options', '/chapter/:chapter_number', controller=optionsController, action = 'OPTIONS', conditions=dict(method=['OPTIONS']))
+    dispatcher.connect('verse_get_options', '/verse/:chapter_number_verse_number', controller=optionsController, action = 'OPTIONS', conditions=dict(method=['OPTIONS']))
+    dispatcher.connect('fav_delete_options', 'favorite/:chapter_number_verse_number', controller=optionsController, action = 'OPTIONS', conditions=dict(method=['OPTIONS']))
+    dispatcher.connect('fav_index_get_options', '/favorite/', controller=optionsController, action = 'OPTIONS', conditions=dict(method=['OPTIONS']))
+    dispatcher.connect('recommedation_get_options', '/recommendation/:number', controller=optionsController, action = 'OPTIONS', conditions=dict(method=['OPTIONS']))
+
+        
 
     conf = {
 	'global': {
             'server.thread_pool': 5, # optional argument
-	    'server.socket_host': 'localhost', # 
-	    'server.socket_port': 51060, #change port number to your assigned
+	    'server.socket_host': 'student10.cse.nd.edu', # 
+	    'server.socket_port': 51026, #change port number to your assigned
 	    },
 	'/': {
 	    'request.dispatch': dispatcher,
+            'tools.CORS.on':True,
 	    }
     }
 
@@ -56,5 +77,6 @@ def start_service():
 
 
 if __name__ == '__main__':
+    cherrypy.tools.CORS = cherrypy.Tool('before_finalize', CORS)
     start_service()
 
