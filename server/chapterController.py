05208ee80554a4cc6cb360fb39c1ff7b50dbe44d
Kai Zhang <kaizhang@Kais-MacBook-Pro.local>
2020-11-08 21:32:58 -0500

    Fixed chapter input bug

--- server/chapterController.py
+++ server/chapterController.py
@@ -12,8 +12,11 @@ class ChapterController(object):
 
     # when GET request for /chapter/chapter_number comes in, then we respond with json string
     def GET_KEY(self, chapter_number):
-    
-        chapter_number = int(chapter_number)
+        try:
+            chapter_number = int(chapter_number)
+        except Exception as ex:
+            return json.dumps({'result': 'error', 'message': 'invalid input'})
+        
         output = {'result':'success', 'chapter_number':chapter_number}
         cid = chapter_number - 1 # note in list, index starts with 0
 
