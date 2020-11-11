d8870e1bb09b363a9110e2e0c1de7ef5e837e615
Kai Zhang <kaizhang@Kais-MacBook-Pro.local>
2020-11-10 22:35:56 -0500

    Update page appearance

--- final_proj/script/fav.js
+++ final_proj/script/fav.js
@@ -6,7 +6,7 @@ var rec_button      = document.getElementById("rec-button");
 var add_button      = document.getElementById("add-button");
 var chapter         = document.getElementById("input-chapter-number");
 var verse           = document.getElementById("input-verse-number");
-
+var list            = document.getElementById("result");
 // show "My Favorite"
 show_my_favorite(); 
 
@@ -15,6 +15,7 @@ add_button.onmouseup     = add_request;
 delete_button.onmouseup  = delete_request;
 clear_button.onmouseup   = clear_request;
 rec_button.onmouseup     = rec_request;
+
 
 //show my favorite Functionality
 function show_my_favorite() {
@@ -35,8 +36,23 @@ function show_my_favorite() {
         if (requester.response["result"] === "error") // error occurs
             alert(requester.response["message"]);
         else {
-            var resp = document.getElementById("answer-label");
-            resp.innerHTML = requester.response['favorite'].map(function(val){return "Ps. " + (val[0] + 1) + ":" + (val[1] + 1) + " | " + val[2]}).join("<br><br>")
+            list.innerHTML = "";
+            var result = requester.response['favorite'];
+            for (var i = 0; i < result.length; i++) {
+                curr = result[i];
+                var curr_li = document.createElement("li");
+                curr_li.innerHTML = "Ps. " + (curr[0] + 1) + ":" + (curr[1] + 1) + " | " + curr[2];
+
+                // create a span object
+                var dataSpan = document.createElement('span');
+                dataSpan.innerHTML = "x";
+                dataSpan.classList.add("close_h");
+                dataSpan.id = (curr[0]+1) + "_" + (curr[1]+1);
+                dataSpan.onclick = close_request;
+                curr_li.appendChild(dataSpan);
+                list.appendChild(curr_li);
+
+            }
         }
 
         console.log("entered on.load()");
@@ -114,8 +130,10 @@ function delete_request() {
 
     requester.onload = function(e) { // triggered when response is received
         
-        if (requester.response["result"] === "error") // error occurs
+        if (requester.response["result"] === "error") {
             alert(requester.response["message"]);
+            return;
+        } // error occurs
         else {
             show_my_favorite() // update my_favorite
             alert("You have deleted " + "Ps. " + String(chapter.value) + ":" + String(verse.value) + ' from "My Favorite" successfully');
@@ -191,6 +209,43 @@ function rec_request() {
             var resp = document.getElementById("recommendation-label");
             resp.innerHTML = requester.response['recommendation'].map(function(val){return "Ps. " + (val[0] + 1) + ":" + (val[1] + 1) + " | " + val[2]}).join("<br><br>")
             alert('The Recommendations for You have been Updated');
+        }
+
+        console.log("entered on.load()");
+    }//end of onload
+
+    requester.onerror = function(e) {
+        console.error(requester.statusText);
+    }//end of onerror
+
+    console.log("message off");
+    requester.send(null); //this actually makes the network call
+
+}
+
+//Close Button Functionality
+function close_request() {
+
+    console.log('close_request');
+    var requester        = new XMLHttpRequest();
+    requester.responseType= "json";
+    var total_url = "http://localhost:" + 51060;
+
+    var pid = this.id;
+
+    total_url = total_url + "/favorite/" + pid;
+    requester.open("DELETE", total_url, true);
+    
+    // set up onload
+    console.log("before requester.onload() in close_request");
+
+    requester.onload = function(e) { // triggered when response is received
+        
+        if (requester.response["result"] === "error") // error occurs
+            alert(requester.response["message"]);
+        else {
+            show_my_favorite() // update my_favorite
+            alert("You have deleted " + "Ps. " + String(chapter.value) + ":" + String(verse.value) + ' from "My Favorite" successfully');
         }
 
         console.log("entered on.load()");
--- final_proj/script/main.js
+++ final_proj/script/main.js
@@ -1,49 +1,49 @@
 console.log("page load - main.js");
 
 var search_button   = document.getElementById("search-button");
+var goto_button     = document.getElementById("goto-button");
 var chapter         = document.getElementById("input-chapter-number");
 var keyword         = document.getElementById("input-keyword");
-var verse           = document.getElementById("input-verse");
+var verse           = document.getElementById("input-verse-number");
 
 //Perform Action Upon Click
-
+goto_button.onmouseup = goto_request;
 search_button.onmouseup  = search_request;
 
+// rec_button.onmouseup     = rec_request;
 
-//Search Button Functionality
-function search_request(){
+//Goto Button Functionality
+function goto_request(){
 
-    console.log('search_request');
+    console.log('goto_request');
     var requester        = new XMLHttpRequest();
     requester.responseType= "json";
     var total_url = "http://localhost:" + 51060;
 
-    if (!chapter.value && !verse.value && !keyword.value) { // Print the entire Psalm
+    if (!chapter.value && !verse.value) { // Print the entire Psalm
         total_url = total_url + "/book/";
         requester.open("GET", total_url, true);
     } 
-    else if (chapter.value && !verse.value && !keyword.value) { // Print a specific chapter
+    else if (chapter.value && !verse.value) { // Print a specific chapter
         total_url = total_url + "/chapter/" + chapter.value;
         requester.open("GET", total_url, true);
     } 
-    else if (chapter.value && verse.value && !keyword.value) { // Print a specific verse from a chapter
+    else if (chapter.value && verse.value) { // Print a specific verse from a chapter
         total_url = total_url + "/verse/" + chapter.value + "_" + verse.value;
         requester.open("GET", total_url, true);
     }
-    else if (!chapter.value && !verse.value && keyword.value){ 
-        total_url = total_url + "/book/" + keyword.value;
-        requester.open("GET", total_url, true);
-    } else {
-        alert("Invalid Input Method.")
+    else {
+        alert("Invalid Input Method.");
+        return;
     }
     
     // set up onload
-    console.log("before xh.onload() in search_request");
+    console.log("before xh.onload() in goto_request");
 
     requester.onload = function(e) { // triggered when response is received
         var resp     = document.getElementById("answer-label");
 
-        if (!chapter.value && !verse.value && !keyword.value) { // Print the entire Psalm
+        if (!chapter.value && !verse.value) { // Print the entire Psalm
             if (requester.response["result"] === "error") { // error occurs
                 alert(requester.response["message"])
                 return
@@ -52,13 +52,14 @@ function search_request(){
             var text = []
             var book_result = requester.response["book"]
             for (var i = 0; i < book_result.length; i++) { // Parse the text of the book
+                if (i != 0) text.push(""); // add an extra line between paragraphs
                 text.push("Chapter " + String(i+1))
                 for (var j = 0; j < book_result[i].length; j++)
                     text.push("Ps. " + String(i+1) + ":" + String(j+1) + " | " + book_result[i][j])
             }
             resp.innerHTML = text.join("<br>")
         } 
-        else if (chapter.value && !verse.value && !keyword.value) { // Print a specific chapter
+        else if (chapter.value && !verse.value) { // Print a specific chapter
             if (requester.response["result"] === "success") {
                 var text = []
                 var chapter_result = requester.response["chapter"]
@@ -69,15 +70,13 @@ function search_request(){
             else
                 alert(requester.response["message"]) 
         } 
-        else if (chapter.value && verse.value && !keyword.value) { // Print a specific verse from a chapter
+        else { // Print a specific verse from a chapter
             if (requester.response["result"] === "success")
                 resp.innerHTML = "Ps. " + String(chapter.value) + ":" + String(verse.value) + " | " + requester.response['verse'];
             else
                 alert(requester.response["message"]) 
         }
-        else if (!chapter.value && !verse.value && keyword.value){ 
-            resp.innerHTML = requester.response['search_result'].map(function(val){return "Ps. " + (val[0] + 1) + ":" + (val[1] + 1) + " | " + val[2]}).join("<br><br>")
-        }
+    
 
         console.log("entered on.load()");
     }//end of onload
@@ -91,5 +90,42 @@ function search_request(){
 
 }
 
+//Search Button Functionality
+function search_request(){
 
+    console.log('search_request');
+    var requester        = new XMLHttpRequest();
+    requester.responseType= "json";
+    var total_url = "http://localhost:" + 51060;
+
+    if (!keyword.value){ 
+        console.log("no keyword input");
+        return;
+    }
+
+    total_url = total_url + "/book/" + keyword.value;
+    requester.open("GET", total_url, true);
+    
+    // set up onload
+    console.log("before xh.onload() in search_request");
+
+    requester.onload = function(e) { // triggered when response is received
+        var resp     = document.getElementById("answer-label");
+
+        if (requester.response["result"] === "success")
+            resp.innerHTML = requester.response['search_result'].map(function(val){return "Ps. " + (val[0] + 1) + ":" + (val[1] + 1) + " | " + val[2]}).join("<br><br>")
+        else
+            alert(requester.response["message"]) 
+
+        console.log("entered on.load()");
+    }//end of onload
+
+    requester.onerror = function(e) {
+        console.error(requester.statusText)
+    }//end of onerror
+
+    console.log("message off");
+    requester.send(null); //this actually makes the network call
+
+}
 
